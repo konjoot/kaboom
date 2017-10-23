1ec1ac83396a7cd3c8b4e7fdcacf57ddf850d94a
Sergey Maksimov <konjoot@gmail.com>
2017-10-23 19:24:06 +0300

    poc: mock service runner

---
+++ cmd/mock.runner.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/konjoot/kaboom/config"
+	"github.com/konjoot/kaboom/mock"
+	grpc "google.golang.org/grpc"
+)
+
+func main() {
+	conf := config.New()
+
+	lis, err := net.Listen("tcp", conf.Listen)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var opts []grpc.ServerOption
+
+	s := grpc.NewServer(opts...)
+
+	mock.RegisterMockServer(s, &mock.Endpoint{})
+
+	s.Serve(lis)
+}
